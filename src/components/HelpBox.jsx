aadd37a7aed05936eb1a43e8680dc5247ee9c65d
Michael Oldacre <65476228+mloldacre@users.noreply.github.com>
2022-11-29 20:26:33 -0500

    changed header tag

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,17 +3,17 @@ import { string } from 'prop-types';
 import './HelpBox.css';
 
 function HelpBox({ title, text }) {
-  return (
-    <article className="help-box">
-      <h3>{title}</h3>
-      <p>{text}</p>
-    </article>
-  );
+	return (
+		<article className='help-box'>
+			<h1>{title}</h1>
+			<p>{text}</p>
+		</article>
+	);
 }
 
 HelpBox.propTypes = {
-  title: string,
-  text: string,
+	title: string,
+	text: string,
 };
 
 export default HelpBox;
